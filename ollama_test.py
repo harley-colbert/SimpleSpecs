821eafbbd4ac3504152717d2219f1fe526cde48c
Harley Colbert <harleycolbert@gmail.com>
2025-10-03 09:34:43 -0400

    headers

---
+++ ollama_test.py
@@ -0,0 +1,20 @@
+import requests
+
+OLLAMA_API = "http://AA-248:11434/api/chat"
+HEADERS = {"Content-Type": "application/json"}
+# MODEL = "deepseek-r1"
+MODEL = "llama3.2"
+# MODEL = "qwen2.5vl"
+
+messages = [
+    {"role": "user", "content": "Tell me a random dad joke."}
+]
+
+payload = {
+        "model": MODEL,
+        "messages": messages,
+        "stream": False,
+    }
+
+response = requests.post(OLLAMA_API, json=payload, headers=HEADERS)
+print(response.json()['message']['content'])
